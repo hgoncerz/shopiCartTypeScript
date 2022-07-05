70ebeb546ffbd9e232fa2f637fe7cb3d90750aca
hgoncerz <hubertg489655@gmail.com>
2022-07-05 10:09:29 +0200

    added functions to app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,34 @@
 import React, { useState } from "react";
 import { useQuery } from "react-query";
 
-const getProducts = async () =>
+export type CartItemType = {
+  id: number;
+  category: string;
+  description: string;
+  image: string;
+  price: number;
+  title: string;
+  amount: number;
+};
+
+const getProducts = async (): Promise<CartItemType[]> =>
   await (await fetch("https://fakestoreapi.com/products")).json();
 
 const App = () => {
+  const { data, isLoading, error } = useQuery<CartItemType[]>(
+    "products",
+    getProducts
+  );
+
+  console.log(data);
+
+  const getTotalItems = () => null;
+
+  const handleAddToCart = () => null;
+
+  const handleRemoveFromCart = () => null;
+
+  if (error) return <div>someting went wrong</div>;
   return <div className="App"></div>;
 };
 
